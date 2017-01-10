d27641ddbfaa656e0099ba88734a3ac5926b9288
Edward Kiefer <edwardkiefer@hotmail.com>
2017-01-10 11:23:42 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,95 @@
+/*
+Using a variable to store a number
+
+var price;
+var quantity;
+var total;
+
+price = 5;
+quantity = 14;
+total = price * quantity;
+
+console.log(total);
+
+
+====================================
+*/
+
+// Strings to store 
+
+/*var username;
+var message;
+username = 'Molly';
+message= "See our range";
+
+var elName = document.getElementById('name');
+elName.textContent = username;
+var elNote = document.getElementById('note');
+elNote.textContent = message;*/
+
+
+// ====================================== //
+
+// Changing value of variable 
+
+
+/*var inStock;
+var shipping;
+
+inStock = true;
+shipping= false;
+
+inStock = false;
+shipping = true;
+
+console.log(inStock); false*/
+    
+// =======================================
+
+// Creating an array and updating
+
+/*var colors =['white',
+             'black',
+             'custom'];
+
+colors[2] = 'red';
+
+
+console.log(colors);*/
+    
+    
+// ======================================
+
+
+// Expressions = evealuates into a single value
+
+//value to variable
+
+/*var color = 'Red';
+
+two or more vaulues to return a single value
+
+var area = 3 * 2;*/
+    
+    
+// ==========================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
